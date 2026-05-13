27e39807f43ba28adfa99960409738e0fc39a08a
NaXeMate <181981244+NaXeMate@users.noreply.github.com>
2026-05-13 11:50:49 +0200

    feat: implement GlobalExceptionHandler to manage exceptions and return structured ErrorDTO responses

---
+++ backend/src/main/java/dev/trainerforge/exception/GlobalExceptionHandler.java
@@ -0,0 +1,61 @@
+package dev.trainerforge.exception;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import dev.trainerforge.dto.response.ErrorDTO;
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(PokedexNotFoundException.class)
+    public ResponseEntity<ErrorDTO> handlePokedexEntryNotFound(
+            PokedexNotFoundException ex, HttpServletRequest request) {
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+            new ErrorDTO(
+                HttpStatus.NOT_FOUND.value(),
+                "Not Found",
+                ex.getMessage(),
+                request.getRequestURI(),
+                LocalDateTime.now()
+            )
+        );
+    }
+
+    @ExceptionHandler(InvalidFilterValueException.class)
+    public ResponseEntity<ErrorDTO> handleInvalidFilterValue(
+            InvalidFilterValueException ex, HttpServletRequest request) {
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+            new ErrorDTO(
+                HttpStatus.BAD_REQUEST.value(),
+                "Bad Request",
+                ex.getMessage(),
+                request.getRequestURI(),
+                LocalDateTime.now()
+            )
+        );
+    }
+
+    // Any other exception that is not handled by the above handlers will be caught here, with a 500 Internal Server Error response.
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDTO> handleGeneric(
+            Exception ex, HttpServletRequest request) {
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+            new ErrorDTO(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Internal Server Error",
+                "An unexpected error occurred",
+                request.getRequestURI(),
+                LocalDateTime.now()
+            )
+        );
+    }
+}
