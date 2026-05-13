53752a4dd759bdfcd4a20b52aa98a6256821a789
NaXeMate <181981244+NaXeMate@users.noreply.github.com>
2026-05-13 18:24:53 +0200

    feat: implement PokedexItemService with methods for retrieving all items and finding by ID

---
+++ backend/src/main/java/dev/trainerforge/service/PokedexItemService.java
@@ -0,0 +1,30 @@
+package dev.trainerforge.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import dev.trainerforge.exception.PokedexNotFoundException;
+import dev.trainerforge.model.entities.PokedexItem;
+import dev.trainerforge.repository.PokedexItemRepository;
+
+@Transactional(readOnly = true)
+@Service
+public class PokedexItemService {
+
+    private final PokedexItemRepository pokedexItemRepo;
+
+    public PokedexItemService(PokedexItemRepository pokedexItemRepo) {
+        this.pokedexItemRepo = pokedexItemRepo;
+    }
+
+    public List<PokedexItem> findAll() {
+        return pokedexItemRepo.findAll();
+    }
+
+    public PokedexItem findById(Long id) {
+        return pokedexItemRepo.findById(id)
+        .orElseThrow(() -> new PokedexNotFoundException(id));
+    }
+}
