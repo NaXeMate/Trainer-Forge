25b97746077992f5d8157930dc8096fe35716770
NaXeMate <181981244+NaXeMate@users.noreply.github.com>
2026-05-29 13:06:20 +0200

    feat: add method to find TypeEffectiveness by attacking and defending types

--- backend/src/main/java/dev/trainerforge/repository/TypeEffectivenessRepository.java
+++ backend/src/main/java/dev/trainerforge/repository/TypeEffectivenessRepository.java
@@ -1,7 +1,11 @@
 package dev.trainerforge.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dev.trainerforge.model.entities.TypeEffectiveness;
 
-public interface TypeEffectivenessRepository extends JpaRepository<TypeEffectiveness, Long> {}
+public interface TypeEffectivenessRepository extends JpaRepository<TypeEffectiveness, Long> {
+    Optional<TypeEffectiveness> findByAttackingTypeAndDefendingType(String attackingTypeName, String defendingTypeName);
+}
